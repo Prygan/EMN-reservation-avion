e0229cb0df7296a82163104ce7dc91ea120108fb
Florine Cerclé <miniflorine@hotmail.fr>
2017-02-03 15:49:15 +0100

    Add database files

---
+++ BDD/insert_catalogue_hotels.sql
@@ -0,0 +1,19 @@
+USE [Hotel]
+DELETE FROM dbo.catalogue_hotels;
+INSERT INTO dbo.catalogue_hotels VALUES
+('Paris', 'Fleur Hotel'),
+('Paris', 'Raplapla Hotel'),
+('Lyon', 'Hirondelle Hotel'),
+('Londres', 'Hullabaloo Hotel'),
+('Londres', 'Wishy-Washy Hotel'),
+('Londres', 'Bumfuzzle Hotel'),
+('Londres', 'Catywampus Hotel'),
+('Berlin', 'Kartoffelnsalad Hotel'),
+('Berlin', 'Geschwindigkeitsbegrenzung Hotel'),
+('Berlin', 'Apfelsaft Hotel'),
+('Tokyo', 'Komorebi Hotel'),
+('Tokyo', 'Shinrin-Yoku Hotel'),
+('Tokyo', 'Ukiyo Hotel'),
+('Dublin', 'Fuddy-Duddy Hotel'),
+('Dublin', 'Leipreachan Hotel'),
+('Edimbourg', 'Squeegee Hotel');
---
+++ BDD/insert_catalogue_vols.sql
@@ -0,0 +1,14 @@
+USE [Vol]
+DELETE FROM dbo.catalogue_vols;
+INSERT INTO dbo.catalogue_vols VALUES
+('Nantes', 'Paris','02:00:00','2017-02-10 10:30:00'),
+('Paris', 'Lyon','03:00:00','2017-20-08 11:20:00'),
+('Strasbourg', 'Londres','01:30:00','2017-03-02 23:15:00'),
+('Edimbourg', 'Berlin','03:45:00','2017-22-05 14:30:00'),
+('Tokyo', 'Paris','12:00:00','2017-14-03 07:45:00'),
+('Marseille', 'Paris','02:00:00','2018-04-03 12:40:00'),
+('Berlin', 'Tokyo','11:00:00','2017-05-03 22:15:00'),
+('Paris', 'Dublin','01:45:00','2017-18-05 18:30:00'),
+('Dublin', 'Edimbourg','01:30:00','2017-20-04 08:00:00'),
+('Londres', 'Berlin','01:00:00','2017-10-02 11:30:00')
+;
