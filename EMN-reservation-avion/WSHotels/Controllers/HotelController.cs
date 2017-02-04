05098076fd273ec793bbb458e328660a65950f34
Florine Cerclé <miniflorine@hotmail.fr>
2017-02-04 20:32:35 +0100

    Add Vol Web Service

--- EMN-reservation-avion/WSHotels/Controllers/HotelController.cs
+++ EMN-reservation-avion/WSHotels/Controllers/HotelController.cs
@@ -9,7 +9,7 @@ using System.Collections;
 using System.Net.Http;
 using System.Net;
 
-namespace WSHotels.Controllers
+namespace WSHotel.Controllers
 {
     public class Hotel
     {
@@ -20,55 +20,48 @@ namespace WSHotels.Controllers
 
     public class Commande
     {
-       // public int ID { get; set; }
+        // public int ID { get; set; }
         public int Hotel { get; set; }
         public String Client { get; set; }
     }
     [Route("api/[controller]")]
     public class HotelController : Controller
     {
-        // GET api/Hotels
+        // GET api/Hotel
         [HttpGet]
         public IEnumerable<Hotel> Get()
         {
-              SqlConnection MyC = new SqlConnection();
-              MyC.ConnectionString = "Data Source=FLORINECERCD9F9\\SQLEXPRESS;Initial Catalog=Hotel;Integrated Security = true";
-              MyC.Open();
-              SqlCommand MyCom = new SqlCommand("select_hotels", MyC);
-             MyCom.CommandType = CommandType.StoredProcedure;
-              IDataReader Reader = MyCom.ExecuteReader();
+            SqlConnection MyC = new SqlConnection();
+            MyC.ConnectionString = "Data Source=FLORINECERCD9F9\\SQLEXPRESS;Initial Catalog=Hotel;Integrated Security = true";
+            MyC.Open();
+            SqlCommand MyCom = new SqlCommand("select_hotels", MyC);
+            MyCom.CommandType = CommandType.StoredProcedure;
+            IDataReader Reader = MyCom.ExecuteReader();
             List<Hotel> Hotels = new List<Hotel>();
             while (Reader.Read())
             {
                 int IDRead = Reader.GetInt32(0);
-                String villeRead = Reader.GetString(1);
-                String nomRead = Reader.GetString(2);
-                Hotels.Add(new Hotel { ID = IDRead, Ville = villeRead, Nom = nomRead });
+                String VilleRead = Reader.GetString(1);
+                String NomRead = Reader.GetString(2);
+                Hotels.Add(new Hotel { ID = IDRead, Ville = VilleRead, Nom = NomRead });
             }
             MyCom.Dispose();
-             MyC.Close();
+            MyC.Close();
 
             return Hotels;
-  
+
         }
 
-        // GET api/Hotels/5
-        [HttpGet("{id}")]
-        public Hotel Get(int id)
-        {
-            return null;
-        }
-
-        // PUT api/Hotels
+        // PUT api/Hotel
         [HttpPut]
         public void Put([FromBody]Commande Commande)
         {
             SqlConnection MyC = new SqlConnection();
             MyC.ConnectionString = "Data Source=FLORINECERCD9F9\\SQLEXPRESS;Initial Catalog=Hotel;Integrated Security = true";
             MyC.Open();
-           
+
             SqlCommand MyCom = new SqlCommand("insert_commande_hotel", MyC);
-           
+
             MyCom.CommandType = CommandType.StoredProcedure;
             MyCom.Parameters.Add("@HOTEL", SqlDbType.Int);
             MyCom.Parameters["@HOTEL"].Value = Commande.Hotel;
@@ -80,16 +73,6 @@ namespace WSHotels.Controllers
             MyC.Close();
         }
 
-        // PUT api/Hotels/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Hotel Hotel)
-        {
-        }
-
-        // DELETE api/Hotels/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      
     }
 }
